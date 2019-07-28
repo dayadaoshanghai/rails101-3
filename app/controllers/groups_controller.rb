b2483767c4f6629b92bd8f6829dcf66eed4008e0
Johnson <974456843@qq.com>
2019-07-28 15:55:19 +0800

    add partial to project

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,7 +3,7 @@ class GroupsController < ApplicationController
   before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
 
   def index
-    @group = Group.all
+    @groups = Group.all
   end
 
   def show
